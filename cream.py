1309b42a7cd4319966d9df73b37819eed0a02afb
SeungHyeon.Bae <rrrwwwlll2013@naver.com>
2025-02-11 12:34:05 +0900

    크림빵

---
+++ cream.py
@@ -0,0 +1,11 @@
+N,K,P=map(int,input().split())
+a=list(map(int,input().split()))
+result = 0
+for i in range(N):
+    count=0
+    for j in range(K):
+        if a[i*K+j] == 0:
+            count = count + 1
+    if P > count:
+        result = result + 1
+print(result)
